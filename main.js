7d5bd373b903bdda8261e5bb46bb11cc10a97072
Vismay Vakharia <vismay.vakharia@tcs.com>
2021-12-18 17:57:23 +0530

    Control UI and canvas changes

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ import { sign } from "./js/utils.js";
 window.sketch = new p5(p5 => { });
 
 sketch.setup = function () {
-    let canvas = sketch.createCanvas(400, 400);
+    let canvas = sketch.createCanvas(sketch.windowWidth, sketch.windowHeight-55);
     canvas.parent("canvas");
     sketch.frameRate(10);
     sketch.textSize(50);
@@ -26,7 +26,7 @@ sketch.draw = function () {
         if (!b.hasAttribute("style")) b.disabled = !sketch.selectionKeys
     });
 
-    sketch.translate(sketch.width / 2, sketch.height / 2);
+    sketch.translate(sketch.width / 2, sketch.game.size*2);
 
     if (sketch.play !== 0) {
         if (sketch.angle < sketch.PI / 2) {
@@ -40,6 +40,10 @@ sketch.draw = function () {
     }
 
     sketch.game.draw(sketch);
+}
+
+sketch.windowResized = function () {
+    sketch.resizeCanvas(sketch.windowWidth, sketch.windowHeight-55);
 }
 
 sketch.keyReleased = function () {
@@ -83,7 +87,7 @@ sketch.keybindings = function (key) {
 
 sketch.mousebindings = function (x, y) {
     let nx = x - sketch.width / 2;
-    let ny = y - sketch.height / 2;
+    let ny = y - sketch.game.size * 2;
     if (sketch.play === 0) {
         let threshold = 100 / 2 + 100 * 2.3 / 2;
         if (nx ** 2 + ny ** 2 < threshold ** 2) {
